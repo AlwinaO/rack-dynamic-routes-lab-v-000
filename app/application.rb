0aaa2147ecf3603152a75459a9ab7a63ed042ece
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-09-08 20:34:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,32 +7,4 @@ class Application
     req = Rack::Request.new(env)
 
 
-    if req.path.match(/testing/).eql?
-    else
-      resp.status = 404
-      resp.write  "Route not found"
-
-
-  # req.path.match(/items/)
-  #     item_price = @@items.collect do |item|
-  #       # binding.pry
-
-
-
-
-    end
-
-    resp.finish
   end
-
-end
-
-
-# req.path.match(/testing/)
-#   resp.status = 404
-#   resp.write  "Route not found"
-
-# @@items.include? do |item|
-#   binding.pry
-#   resp.status = 404
-#   resp.write  "Route not found"
