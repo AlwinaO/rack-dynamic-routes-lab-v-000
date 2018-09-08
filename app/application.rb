424cda3f052ff4c979765161817afbe2656c43ba
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-09-08 21:18:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,14 +9,15 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
         # binding.pry
-      item = @@items.find{|p| p.name == item_name}
-      resp.write item.price
-
+      if item = @@items.find{|p| p.name == item_name}
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
-
-
     end
 
     resp.finish
